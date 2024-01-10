9cdf09796cf1842377a329e9860cbcf2489c2a43
dlwlckdWkd12 <jccc23@naver.com>
2024-01-10 18:52:09 +0900

    0110

--- src/test/notepad.java
+++ src/test/notepad.java
@@ -6,21 +6,16 @@ public class notepad {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
         int n = scanner.nextInt();
 
-        for(int i = 0; i < n ; i++){
-
-            int A =scanner.nextInt();
-            int B =scanner.nextInt();
-            System.out.println(A+B);
-
+        int sum = 0;
+        for(int i = 1; i <=n ; i++){
+            sum += i;
         }
+
+        System.out.println(sum);
     }
 
+
 }
-
-
-
-//    첫째 줄에 테스트 케이스의 개수 T가 주어진다.
-
-//각 테스트 케이스는 한 줄로 이루어져 있으며, 각 줄에 A와 B가 주어진다. (0 < A, B < 10)
