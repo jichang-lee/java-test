da7118c83947d3f6bbf2ad5653deaf1c7793a649
dlwlckdWkd12 <jccc23@naver.com>
2024-01-10 18:10:05 +0900

    0110

--- src/test/notepad.java
+++ src/test/notepad.java
@@ -4,7 +4,18 @@ import java.util.Scanner;
 
 public class notepad {
 
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
 
+        for(int i = 0; i < n ; i++){
+
+            int A =scanner.nextInt();
+            int B =scanner.nextInt();
+            System.out.println(A+B);
+
+        }
+    }
 
 }
 
