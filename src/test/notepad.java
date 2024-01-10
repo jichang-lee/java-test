bab34be503e3f1c535f07e08ff22fa5ab3fbabc5
dlwlckdWkd12 <jccc23@naver.com>
2024-01-11 05:50:07 +0900

    0111

--- src/test/notepad.java
+++ src/test/notepad.java
@@ -6,24 +6,15 @@ public class notepad {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int result = 0 ;
+        int a = scanner.nextInt();
+        String str = "";
 
-        int totalPrice = scanner.nextInt();
-        int product = scanner.nextInt();
-
-        for(int i = 0 ; i<product ; i++){
-            int a = scanner.nextInt();
-            int b = scanner.nextInt();
-            System.out.println(a + " " + b);
-            result += a * b;
+        int numLong = a/4;
+        for(int i = 0 ; i <numLong ; i++){
+            str += "long ";
         }
+            System.out.println(str + "int");
 
-        if(totalPrice == result){
-            System.out.println("Yes");
-        }else{
-            System.out.println("No");
-
-        }
 
     }
 
