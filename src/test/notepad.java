8153fb96b8d8d939f91078619890dcc51dc43934
dlwlckdWkd12 <jccc23@naver.com>
2024-01-11 17:06:09 +0900

    0111

--- src/test/notepad.java
+++ src/test/notepad.java
@@ -7,19 +7,21 @@ public class notepad {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
 
         int N = Integer.parseInt(br.readLine());
 
         StringTokenizer st;
 
-        for (int j = 0; j < N; j++) {
+        for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine()," ");
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            System.out.println(a+b);
+            bw.write((Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken()))+ "\n");
         }
-
         br.close();
+
+        bw.flush();
+        bw.close();
 
     }
 
